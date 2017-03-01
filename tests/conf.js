6f77e33eecabf0041730b49cdd174721caa3f7b9
Maryam Ghanizadeh <maloochpich@gmail.com>
2017-03-01 13:45:16 -0600

    Update conf.js
    remove extra lines

--- tests/conf.js
+++ tests/conf.js
@@ -1,15 +1,3 @@
-//exports.config = {
-//  seleniumAddress: 'http://localhost:4444/wd/hub',
-//  specs: ['test.js'],
-//  jasmineNodeOpts: {
-//      onComplete: null,
-//      isVerbose: false,
-//      showColors: true,
-//      includeStackTrace: false
-//  }
-//};
-
-
 exports.config = {
     framework: 'mocha',
     seleniumAddress: 'http://localhost:4444/wd/hub',
